d04f84c8fc3f29cc81f30087530d962ef725a1c0
Jeff Voight <jeff@voight.org>
2021-12-24 14:02:57 -0500

    using own docker image now.

--- packer/build.sh
+++ packer/build.sh
@@ -3,11 +3,15 @@
 export password=`openssl rand -base64 9`
 
 echo $password
-
+epoch=`date +%s`
+export ksisoname="ks-proxmox-${epoch}.iso"
+export templateName="copper-centos7-${epoch}"
 hash=`openssl passwd -6 $password`
 
 sed -i -E "s|\-\-password=(.*)|--password=$hash|g" http/ks-proxmox.cfg
-
+mkisofs -o "${ksisoname}" http
+export ksisochecksum=`sha256sum ${ksisoname}|awk '{print $1}'`
+packer init proxmox.pkr.hcl
 packer build --force proxmox.pkr.hcl
-
+rm -f "${ksisoname}"
 sed -i -E "s|\-\-password=(.*)|--password=randpass|g" http/ks-proxmox.cfg
